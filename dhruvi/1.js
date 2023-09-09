81c9fdabb92dc35053b642f66368ea3addb4f085
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 12:04:24 +0530

    1 file modified

--- dhruvi/1.js
+++ dhruvi/1.js
@@ -1,12 +1,12 @@
 // ***10
-function summation(a,b,c,d,e){
-  let i=(a+b+c+d+e);
-  return i;
-}
-let a=Number(prompt('enter value a'));
-let b=Number(prompt('enter value b'));
-let c=Number(prompt('enter value c'));
-let d=Number(prompt('enter value d'));
-let e=Number(prompt('enter value e'));
-let ans=summation(a,b,c,d,e);
-console.log(ans);
+// function summation(a,b,c,d,e){
+//   let i=(a+b+c+d+e);
+//   return i;
+// }
+// let a=Number(prompt('enter value a'));
+// let b=Number(prompt('enter value b'));
+// let c=Number(prompt('enter value c'));
+// let d=Number(prompt('enter value d'));
+// let e=Number(prompt('enter value e'));
+// let ans=summation(a,b,c,d,e);
+// console.log(ans);
