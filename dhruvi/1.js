8b4ffc94c49a0aceaf511a060a2056eaa9e002fe
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-09-09 12:10:44 +0530

    1 file modified

--- dhruvi/1.js
+++ dhruvi/1.js
@@ -3,10 +3,27 @@
 //   let i=(a+b+c+d+e);
 //   return i;
 // }
+
+// function subtraction(a,b,c,d,e){
+//     let i=(a-b-c-d-e);
+//     return i;
+//   }
+//   let ans=subtraction(a,b,c,d,e);
+
+// function multiplication(a,b,c,d,e){
+//       let i=(a*b*c*d*e);
+//       return i;
+//     }
+// let ans=multiplication(a,b,c,d,e);
+
+function division(a,b){
+  let i=(a/b);
+  return i;
+}
 // let a=Number(prompt('enter value a'));
 // let b=Number(prompt('enter value b'));
 // let c=Number(prompt('enter value c'));
 // let d=Number(prompt('enter value d'));
 // let e=Number(prompt('enter value e'));
-// let ans=summation(a,b,c,d,e);
+let ans=division(a,b)
 // console.log(ans);
